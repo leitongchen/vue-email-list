79903e9ef70c662309ea15f294575aa9c2e826e5
leitong <leitongchen93@gmail.com>
2021-05-11 16:21:20 +0200

    Funzione onClickMail genera 10 emails casuali

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,34 @@
 new Vue({
     el: "#app",
     data: {
-
+        generatedMail: null,
+        emailList: [],
     },
     methods: {
-        
+        onClickMail() {
+
+            const getMailLoops = 10; 
+            let arrayOfMail = []
+            
+            for (let i=0; i < getMailLoops; i++) {
+                
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+                    //console.log(resp)
+    
+                    this.generatedMail = resp.data.response
+
+                    arrayOfMail.push(resp.data.response)
+                    //console.log(arrayOfMail)
+
+                    if (arrayOfMail.length === getMailLoops) {
+
+                         
+                        this.emailList.push(...arrayOfMail);
+                        console.log(`email list ${arrayOfMail}`)
+                    }
+                })
+            }
+
+        }
     }
 })
